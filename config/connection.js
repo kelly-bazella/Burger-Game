06f73e0ffda52829dbe4f63d5c4f0bac7dd4bd58
Kelly Bazella <kelly.bazella@gmail.com>
2020-03-26 18:23:23 -0500

    heroku connect

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
 
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    var connection = mysql.createConnection({
+        host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "m3x2vtkwq6fqe7b2",
+        password: "bk808gw6l85qtq5u",
+        database: "nmyjxxlv6dmfz17n"
+    });
+}
 connection.connect(function(err){
     if(err){
         console.log("error connecting: "+ err.stack);
